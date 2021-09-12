a8b56bd1c43610c185f9ad833a366cbf4a4ed61d
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-09-12 12:03:01 +0530

    Done with Creating HomePage and React Fixed Navbar

---
+++ my-app/src/Home/Home.jsx
@@ -0,0 +1,178 @@
+import "./Home.scss";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://img.wallpapersafari.com/desktop/1600/900/1/69/Z8QOjH.jpg"
+        alt=""
+      />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+      dolores, temporibus, ducimus, voluptate eligendi doloremque sed tempora
+      eos quaerat quidem soluta placeat atque? Accusantium, qui commodi
+      consequatur amet quaerat inventore. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Repellendus dolores, temporibus, ducimus, voluptate
+      eligendi doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Repellendus dolores,
+      temporibus, ducimus, voluptate eligendi doloremque sed tempora eos quaerat
+      quidem soluta placeat atque? Accusantium, qui commodi consequatur amet
+      quaerat inventore. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Repellendus dolores, temporibus, ducimus, voluptate eligendi
+      doloremque sed tempora eos quaerat quidem soluta placeat atque?
+      Accusantium, qui commodi consequatur amet quaerat inventore.
+    </div>
+  );
+};
+
+export default Home;
---
+++ my-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import "./Navbar.scss";
+import Search from "@material-ui/icons/Search";
+import { ArrowDropDown, Notifications } from "@material-ui/icons";
+import { useState } from "react";
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => window.onscroll == null;
+  };
+  console.log(isScrolled);
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            // src="https://wallpapercave.com/wp/wp7478420.jpg"
+            alt=""
+          />
+          <span>Home</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?size=626&ext=jpg"
+            alt=""
+          />
+
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>settings</span>
+              <span>logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
